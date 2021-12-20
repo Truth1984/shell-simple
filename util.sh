74ce14da86231e9dd9ab1a4643bafbb03d93b546
truth1984 <zhengyao365@163.com>
2021-12-20 10:15:39 +0800

    add dockerfile

--- util.sh
+++ util.sh
@@ -23,7 +23,7 @@ _UTILDATE() {
 _RC() {
     if [ "$verbose" == 1 ]; then
         echo L1, RC, $1, $(_UTILDATE), \<${FUNCNAME[ 1 ]}\> >&2
-        elif [ "$verbose" == 2 ] || [ "$verbose" == 3 ]  ; then
+    elif [ "$verbose" == 2 ] || [ "$verbose" == 3 ]  ; then
         echo L$verbose, RC, $1, $(_UTILDATE), \<${FUNCNAME[ 1 ]}\>, \("${@:2:$#}"\) >&2
     fi;
     return $1
@@ -34,7 +34,7 @@ _RC() {
 _ERC() {
     if [ "$verbose" == 1 ]; then
         echo L1, ERC, 1, $(_UTILDATE), \<${FUNCNAME[ 1 ]}\>, "<x "$1" x>" >&2
-        elif [ "$verbose" == 2 ] || [ "$verbose" == 3 ]  ; then
+    elif [ "$verbose" == 2 ] || [ "$verbose" == 3 ]  ; then
         echo L$verbose, ERC, 1, $(_UTILDATE), \<${FUNCNAME[ 1 ]}\>, \("${@:2:$#}"\), "<x "$1" x>" >&2
     fi;
     return 1
@@ -45,9 +45,9 @@ _ERC() {
 _EC() {
     if [ "$verbose" == 1 ]; then
         echo L1, EC, 0, $(_UTILDATE), \<${FUNCNAME[ 1 ]}\> >&2
-        elif [ "$verbose" == 2 ]; then
+    elif [ "$verbose" == 2 ]; then
         echo L2, EC, 0, $(_UTILDATE), \<${FUNCNAME[ 1 ]}\>, \("${@:2:$#}"\) >&2
-        elif [ "$verbose" == 3 ]; then
+    elif [ "$verbose" == 3 ]; then
         echo L3, EC, 0, $(_UTILDATE), \<${FUNCNAME[ 1 ]}\>, \("${@:2:$#}"\), \"\["$1"\]\" >&2
     fi;
     echo $1
@@ -156,12 +156,12 @@ uuid() {
 # (): string
 pkgManager() {
     if $(hasCmd yum); then _EC "yum";
-        elif $(hasCmd brew); then _EC "brew";
-        elif $(hasCmd apt); then _EC "apt";
-        elif $(hasCmd apk); then _EC "apk";
-        elif $(hasCmd dnf); then _EC "dnf";
-        elif $(hasCmd winget); then _EC "winget";
-        elif $(hasCmd choco); then _EC "choco";
+    elif $(hasCmd brew); then _EC "brew";
+    elif $(hasCmd apt); then _EC "apt";
+    elif $(hasCmd apk); then _EC "apk";
+    elif $(hasCmd dnf); then _EC "dnf";
+    elif $(hasCmd winget); then _EC "winget";
+    elif $(hasCmd choco); then _EC "choco";
     fi;
 }
 
@@ -171,12 +171,12 @@ install() {
     if $(osCheck linux) && $(hasCmd sudo); then prefix=$prefix."sudo "; fi;
     
     if $(stringEqual $m yum); then eval $(_EC "$prefix yum install -y $@");
-        elif $(stringEqual $m brew); then eval $(_EC "HOMEBREW_NO_AUTO_UPDATE=1 brew install $@");
-        elif $(stringEqual $m apt); then eval $(_EC "$prefix apt-get install -y $@");
-        elif $(stringEqual $m apk); then eval $(_EC "$prefix apk add $@");
-        elif $(stringEqual $m dnf); then eval $(_EC "$prefix dnf install -y $@");
-        elif $(stringEqual $m winget); then eval $(_EC "winget --accept-package-agreements --accept-source-agreements install $@");
-        elif $(stringEqual $m choco); then eval $(_EC "choco install -y $@");
+    elif $(stringEqual $m brew); then eval $(_EC "HOMEBREW_NO_AUTO_UPDATE=1 brew install $@");
+    elif $(stringEqual $m apt); then eval $(_EC "$prefix apt-get install -y $@");
+    elif $(stringEqual $m apk); then eval $(_EC "$prefix apk add $@");
+    elif $(stringEqual $m dnf); then eval $(_EC "$prefix dnf install -y $@");
+    elif $(stringEqual $m winget); then eval $(_EC "winget --accept-package-agreements --accept-source-agreements install $@");
+    elif $(stringEqual $m choco); then eval $(_EC "choco install -y $@");
     fi;
 }
 
@@ -219,6 +219,31 @@ stringEqual() {
 searchFile() {
     local base=${!2:="."}
     find $base -name $1
+}
+
+# (...pkgname)
+dockerfile() {
+
+    setup
+
+    if $(osCheck yum); then
+        yum install -y epel-release nano net-tools curl redhat-lsb-core supervisor
+        yum install -y ${@}
+        yum clean all 
+    fi;
+
+    if $(osCheck apk); then
+        apk add --no-cache nano curl supervisor
+        apk add --no-cache ${@}
+    fi;
+
+    if $(osCheck apt); then
+        apt-get -qq update  
+        apt-get -qq --no-install-recommends install nano curl wget net-tools supervisor 
+        apt-get -qq --no-install-recommends install ${@}
+        apt-get -qq clean && rm -rf /var/lib/apt/lists/*
+    fi;
+    
 }
 
 setup() {
